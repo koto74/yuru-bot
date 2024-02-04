ae7b83067ba99e551b3698cbdc739898c91421a6
koto74 <kotoganaiyo@gmail.com>
2024-02-04 18:59:16 +0900

    chore: デバッグ用コード削除

--- bot/src/packages/event_module.py
+++ bot/src/packages/event_module.py
@@ -5,7 +5,6 @@ event_functions = {
 }
 
 async def message_handler(message):
-    print(message.content)
     event_function = event_functions.get(message.content, None)
     if event_function != None:
         await event_function(message)
