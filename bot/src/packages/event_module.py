82bec25e3b5a9a81da2f7c738a3113c76859d6df
koto74 <kotoganaiyo@gmail.com>
2024-02-23 22:51:09 +0900

    feat: 愚地独歩名言実装

--- bot/src/packages/event_module.py
+++ bot/src/packages/event_module.py
@@ -1,7 +1,9 @@
-from .hello import handle_hello
+from .messages.hello import handle_hello
+from .messages.orochi import handle_orochi
 
 event_functions = {
     "hello": handle_hello,
+    "名言": handle_orochi,
 }
 
 async def message_handler(message):
